4b8473aed1310d84b363cfd087ade350becfc3ab
Amrit <singhamritf@gmail.com>
2025-11-19 18:04:46 +0530

    development branch create

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import { ReactNode } from 'react';
 
 export const metadata = {
   title: 'Venejobs',
-  description: 'Conecta con freelancers talentosos de Venezuela para potenciar tu negocio.',
+  description: 'Conecta con freelancers talentosos de Venezuela para potenciar tu negocio.!',
 };
 
 export default function RootLayout({ children }: { children: ReactNode }) {
