364dbdee714b01c01f4368e4c6779a1598e24f51
AmritSingh <singhamritf@gmail.com>
2025-12-05 22:43:06 +0530

    resolve conflict

--- src/app/components/ActiveContracts/ContractCard.jsx
+++ src/app/components/ActiveContracts/ContractCard.jsx
@@ -4,7 +4,7 @@ import Button from "../button/Button";
 export default function ContractCard() {
     return (
         <div className="border-b border-[rgba(68,68,68,0.08)] p-5 lg:p-8">
-            <div className="w-full flex flex-col lg:flex-row items-start lg:items-center rounded justify-between gap-6">
+            <div className="w-full flex flex-col lg:flex-row items-start lg:items-center justify-between gap-6">
 
                 {/* Left Section */}
                 <div className="flex items-center gap-6 w-full lg:w-auto">
@@ -16,11 +16,11 @@ export default function ContractCard() {
                         className="rounded-full w-16 h-16"
                     />
                     <div className="flex flex-col gap-1.5">
-                        <div className="flex flex-row sm:flex-row sm:items-center gap-2 lg:gap-4">
-                            <h3 className="text-xl lg:text-2xl text-heading font-semibold tracking-normal">
+                        <div className="flex flex-row items-center gap-2 lg:gap-4">
+                            <h3 className="text-xl lg:text-2xl text-heading font-semibold">
                                 Harnish
                             </h3>
-                            <p className="bg-secondary text-white rounded-3xl border font-medium px-4 py-1 text-xs lg:text-sm w-fit">
+                            <p className="bg-secondary text-white rounded-3xl px-4 py-1 text-xs lg:text-sm w-fit">
                                 Active Contract
                             </p>
                         </div>
@@ -41,7 +41,7 @@ export default function ContractCard() {
                 </div>
 
                 {/* Amounts */}
-                <div className="flex flex-col sm:flex-row lg:flex-row items-start sm:items-center gap-5 lg:gap-30 w-full lg:w-auto">
+                <div className="flex flex-col sm:flex-row items-start sm:items-center gap-5 lg:gap-30 w-full lg:w-auto">
                     <div className="flex flex-col gap-2">
                         <h3 className="text-heading font-semibold text-sm md:text-lg">
                             Contract Amount :
@@ -66,44 +66,6 @@ export default function ContractCard() {
                     View contract
                 </Button>
             </div>
-
-            <p className="text-paragraph text-sm lg:text-base font-medium">
-              Can u design ui
-            </p>
-
-            <div className="flex flex-row items-center">
-              <p className="text-heading text-sm lg:text-base font-medium">
-                Started Date:&nbsp;
-              </p>
-              <p className="text-paragraph text-sm lg:text-base font-medium">
-                11-01-0001
-              </p>
-            </div>
-          </div>
         </div>
-
-        {/* Amounts */}
-        <div className="flex flex-col sm:flex-row lg:flex-row items-start sm:items-center gap-5 lg:gap-30 w-full lg:w-auto">
-          <div className="flex flex-col gap-2">
-            <h3 className="text-heading font-semibold text-sm md:text-lg">
-              Contract Amount :
-            </h3>
-            <p className="text-paragraph font-medium text-base">$1000.00</p>
-          </div>
-
-          <div className="flex flex-col gap-2">
-            <h3 className="text-heading font-semibold text-sm md:text-lg">
-              Paid Amount :
-            </h3>
-            <p className="text-paragraph font-medium text-base">$500.00</p>
-          </div>
-        </div>
-
-        {/* View Button */}
-        <Button className="bg-primary text-white px-4 rounded  sm:w-auto">
-          View contract
-        </Button>
-      </div>
-    </div>
-  );
+    );
 }
