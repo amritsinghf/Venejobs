8a88f54b546da5431cfd96f04c1b55437fac630e
AmritSingh <singhamritf@gmail.com>
2025-12-06 11:29:18 +0530

    Fix the job list page ui

--- src/app/components/jobs/JobCard.jsx
+++ src/app/components/jobs/JobCard.jsx
@@ -20,7 +20,7 @@ export default function JobCard({ item }) {
     <div className="border-b border-[rgba(68,68,68,0.08)] p-5 lg:p-8">
       <div className="w-full flex flex-col xl:flex-row rounded justify-between gap-5 lg:gap-3.5">
         <div className="flex flex-col gap-5 lg:gap-3.5">
-          <h3 className="text-xl lg:text-2xl text-heading font-semibold tracking-normal">
+          <h3 className="text-xl xl:text-2xl text-heading font-semibold tracking-normal">
             {item.title}
           </h3>
 
