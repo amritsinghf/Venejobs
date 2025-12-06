376aef2c163d45e7931140fd3493fd1d450bc6ed
AmritSingh <singhamritf@gmail.com>
2025-12-06 10:46:27 +0530

    Fix the ui

--- src/app/client/jobpost/page.jsx
+++ src/app/client/jobpost/page.jsx
@@ -19,7 +19,7 @@ export default function Jobpost() {
   return (
     <>
       <ClientLayout>
-        <div className="w-full max-w-[90%] sm:max-w-[540px] md:max-w-[720px] lg:max-w-[960px] xl:max-w-[1240px] 2xl:max-w-[1400px] mx-auto my-10 lg:my-20">
+        <div className="w-full lg:max-w-[960px] xl:max-w-[1240px] 2xl:max-w-[1600px] mx-auto mx-auto my-10 lg:my-20">
           <div className="flex flex-col gap-5 lg:gap-8">
 
             <h1 className="text-3xl lg:text-5xl text-heading font-bold leading-snug w-full lg:max-w-5xl">
