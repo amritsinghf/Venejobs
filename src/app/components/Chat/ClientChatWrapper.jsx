ac453618a69639913a1486009e0ffd4a72116633
AmritSingh <singhamritf@gmail.com>
2025-12-13 15:49:42 +0530

    Fix the chat ui

--- src/app/components/Chat/ClientChatWrapper.jsx
+++ src/app/components/Chat/ClientChatWrapper.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Mobileview from "./MobileViewChatList";
 
-const ClinetChatWrapper = ({ children, chatData }) => {
+const ClientChatWrapper = ({ chatData = [], children }) => {
   return (
     <div>
       <div className="w-full max-w-[90%] sm:max-w-[540px] md:max-w-[720px] lg:max-w-[960px] xl:max-w-[1240px] 2xl:max-w-[1400px] mx-auto my-10 lg:my-20">
@@ -23,4 +23,4 @@ const ClinetChatWrapper = ({ children, chatData }) => {
   );
 };
 
-export default ClinetChatWrapper;
+export default ClientChatWrapper;
